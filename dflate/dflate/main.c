584c7f9ec5e69e60fec0115c782580fbc5a0c75a
David Kopec <david@oaksnow.com>
2020-09-29 01:02:17 -0400

    add

--- dflate/dflate/main.c
+++ dflate/dflate/main.c
@@ -40,6 +40,7 @@ typedef struct {
     char *FCOMMENT;
     uint16_t FHCRC;
     uint8_t *data;
+    size_t data_length;
     uint32_t CRC32;
     uint32_t ISIZE;
 } gzipfile;
@@ -63,10 +64,73 @@ uint8_t *dflate(void *original) {
     return compressed;
 }
 
-uint8_t *nflate(void *compressed) {
+typedef struct {
+    uint8_t *data;
+    size_t byteLength;
+    uint64_t bitIndex;
+} bitstream;
+
+bitstream *create_bitstream(uint8_t *data, size_t length) {
+    bitstream *bs = calloc(1, sizeof(bitstream));
+    bs->data = data;
+    bs->byteLength = length;
+    return bs;
+}
+
+bool read_bit(bitstream *bs) {
+    return ((bs->data[bs->bitIndex / 8]) >> (7 - (bs->bitIndex++ % 8))) & 1;
+}
+
+//read up to 64 bits at a time
+uint64_t read_bits(bitstream *bs, int n) {
+    uint64_t bits = 0;
+    for (int i = 0; i < n; i++) {
+        bits = (bits << 1) | read_bit(bs);
+    }
+    return bits;
+}
+
+uint8_t *nflate(uint8_t *compressed, size_t length, size_t *result_length) {
     uint8_t *reconstituted = NULL;
     
+    bitstream *bs = create_bitstream(compressed, length);
+    
+    // read block header
+    bool BFINAL = read_bit(bs); // is this the last block?
+    uint64_t BTYPE = read_bits(bs, 2);
+    
     return reconstituted;
+}
+
+// https://en.wikipedia.org/wiki/Cyclic_redundancy_check#CRC-32_algorithm
+bool doCRC32Check(uint8_t *data, size_t length, uint32_t crc_check) {
+    uint32_t crc32 = 0xFFFFFFFF;
+    uint32_t lookup_table[256];
+    
+    // code from RFC 1952 for table
+    // https://tools.ietf.org/html/rfc1952#section-8.1.1.6.2
+    uint32_t c;
+
+    int n, k;
+    for (n = 0; n < 256; n++) {
+        c = (uint32_t) n;
+        for (k = 0; k < 8; k++) {
+            if (c & 1) {
+                c = 0xedb88320 ^ (c >> 1);
+            } else {
+                c = c >> 1;
+            }
+        }
+        lookup_table[n] = c;
+    }
+    
+    // based on Wikipedia pseudocode
+    for (int i = 0; i < length; i++) {
+        int lookup_index = (crc32 ^ data[i]) & 0xFF;
+        crc32 = (crc32 >> 8) ^ lookup_table[lookup_index];
+    }
+    crc32 ^= 0xFFFFFFFF;
+    return crc32 == crc_check;
 }
 
 gzipfile *read_gzipfile(const char *name) {
@@ -152,9 +216,10 @@ gzipfile *read_gzipfile(const char *name) {
         fprintf(stderr, "Error seeking to end of data block.");
     }
     long int data_end = ftell(input);
-    fseek(input, 0, (int)data_start);
+    fseek(input, data_start, SEEK_SET);
     long int data_size = data_end - data_start;
     gzf->data = malloc(data_size);
+    gzf->data_length = data_size;
     fread(gzf->data, data_size, 1, input);
     fread(&gzf->CRC32, 4, 1, input);
     fread(&gzf->ISIZE, 4, 1, input);
@@ -179,6 +244,23 @@ int main(int argc, const char * argv[]) {
         return 1;
     }
     
+    size_t uncompressed_length = 0;
+    uint8_t *uncompressed = nflate(gzf->data, gzf->data_length, &uncompressed_length);
+    
+//    uint8_t data[2] = {0b10001000, 0b10001111};
+//    bitstream *bs = create_bitstream(data, 2);
+//    uint64_t bits1 = read_bits(bs, 3);
+//    uint64_t bits2 = read_bits(bs, 3);
+//    uint64_t bits3 = read_bits(bs, 3);
+//    uint64_t bits4 = read_bits(bs, 4);
+//    uint64_t bits5 = read_bits(bs, 3);
+//    bs->bitIndex = 0;
+//    uint64_t bits6 = read_bits(bs, 4);
+//    uint64_t bits7 = read_bits(bs, 10);
+    // CRC is on uncompressed data
+//    if (!doCRC32Check(gzf->data, gzf->data_length, gzf->CRC32)) {
+//        fprintf(stderr, "CRC32 check did not pass on data.");
+//    }
     
     free_gzfipfile(gzf);
     return 0;
